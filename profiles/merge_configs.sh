49432e41150bb5cf2996ef1885b6236d9cbcf72c
Your Name <you@example.com>
2021-09-06 10:55:36 -0700

    replacing KDE hardened with Sea

--- profiles/merge_configs.sh
+++ profiles/merge_configs.sh
@@ -1,3 +1,3 @@
 src=$1
 dir=$2
-rsync -avP $src $dir --exclude={airootfs/root/customize_airootfs.sh,packages.x86_64,profiledef.sh} $@
+rsync -avP $src/ $dir/ --exclude={airootfs/root/customize_airootfs.sh,packages.x86_64} $@
